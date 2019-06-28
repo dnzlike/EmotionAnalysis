b6eec537fa2dd0f54fe2ac14363d0ac9b9c38c6e
dnzlike <dnz7993@sjtu.edu.cn>
2019-06-28 12:52:51 +0800

    Update model and combined

--- word2vec/word2vec.py
+++ word2vec/word2vec.py
@@ -3,12 +3,15 @@ import codecs
 import jieba
 import jieba.analyse
 import numpy as np
+import multiprocessing
 from gensim.models import word2vec
 
-stopwords = {}.fromkeys(['的', '和', '是','\n', '-', '1'])
-negPath = '/Python/SentimentAnalysis/neg.txt'
-posPath = '/Python/SentimentAnalysis/pos.txt'
+stopwords = {}.fromkeys(['\n', '\''])
+negPath = './10000/neg/neg.'
+posPath = './10000/pos/pos.'
 respath = '/Python/SentimentAnalysis/result.txt'
+NEG = 2999
+POS = 6999
 
 def cut_stopwords(line):
     res = ''
@@ -19,33 +22,33 @@ def cut_stopwords(line):
 
 
 def read_file_cut():
-    comment = ''
-    fileName = negPath
-    source = open(fileName, encoding = 'utf-8')
-    line = source.read()
-    line = cut_stopwords(line)
-    seglist = jieba.cut(line)
-    output = ' '.join(seglist)
-    comment += output
-    fileName = posPath
-    source = open(fileName, encoding = 'utf-8')
-    line = source.read()
-    line = cut_stopwords(line)
-    seglist = jieba.cut(line)
-    output = ' '.join(seglist)
-    comment += output
-    source.close()
-    
-    resName = respath
-    if os.path.exists(resName):
-            os.remove(resName)
-    result = codecs.open(resName, 'w', encoding = 'utf-8')
-    result.write(comment)
-    result.close()
+    neg = 0
+    pos = 0
+    text = []
+    while neg <= NEG:
+        fileName = negPath + str(neg) + '.txt'
+        source = open(fileName, encoding = 'utf-8')
+        line = source.read()
+        line = cut_stopwords(line)
+        text.append(line)
+        source.close()
+        neg = neg + 1
+    while pos <= POS:
+        fileName = posPath + str(pos) + '.txt'
+        source = open(fileName, encoding = 'utf-8')
+        line = source.read()
+        line = cut_stopwords(line)
+        text.append(line)
+        source.close()
+        pos = pos + 1
+    text = [jieba.lcut(document.replace('\n', '')) for document in text]
+    print(text[:10])
+    print(len(text))
+    return text
 
 def model_train(model_name):
     sentences = word2vec.Text8Corpus(respath)
-    model=word2vec.Word2Vec(sentences,size=200)
+    model=word2vec.Word2Vec(sentences,size=100, min_count=10, window=7, iter=1)
     model.save(model_name)
 	# model.wv.save_word2vec_format(model_name + ".bin", binary=True)
 
@@ -54,18 +57,21 @@ def model_to_list(model):
     wordlist = wordvec.index2word
     wvs = []
     wlen = len(wordlist)
-    wvs = np.zeros((wlen, 200), dtype = 'float32')
+    wvs = np.zeros((wlen, 100), dtype = 'float32')
     for i in range(len(wordlist)):
         a = wordlist[i]
         wvs[i] = model.wv[a]
     return wordlist, wvs
 
+
+
 if __name__ == '__main__':
-#     read_file_cut()
-    model_name = 'word2vec.model'
+    text = read_file_cut()
+    
+    '''model_name = 'word2vec.model'
     if not os.path.exists(model_name):
         model_train(model_name)
     model = word2vec.Word2Vec.load(model_name)
     wordlist, wvs = model_to_list(model)
     for i in range(10):
-        print(wordlist[i], wvs[i])
+        print(wordlist[i], wvs[i])'''
